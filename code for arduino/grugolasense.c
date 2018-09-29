4173154b2ff53ca8a376afa9cc450cd77e712c30
Golpe82 <33048138+Golpe82@users.noreply.github.com>
2018-09-29 20:37:57 +0200

    grugolasense

---
+++ code for arduino/grugolasense.c
@@ -0,0 +1,28 @@
+//grugolasense.c
+
+/*The Grugola
+  The Grugola should be a Lawnmower created by
+  Frank Lautenschlaeger, Daniel Grunow and
+  Simon Golpe Varela.
+  It is a end projekt for the "state technician school- Berlin"
+  This code is for the sensor part of the robot and was created
+  by Simon Golpe Varela on 09/26/2018.
+  Last change: 09/26/2018
+*/
+
+#include <Arduino.h>
+#include <stdio.h>
+#include "grugolasense.h"
+
+
+int blinky()
+{
+  int blinkstate;
+  blinkstate=digitalRead(LED_BUILTIN);
+  digitalWrite(LED_BUILTIN, LOW);   
+  delay(1000);                      
+  digitalWrite(LED_BUILTIN, HIGH);
+
+  return blinkstate;
+}
+
---
+++ code for arduino/grugolasense.h
@@ -0,0 +1,30 @@
+//grugolasense.h
+
+/*The Grugola
+  The Grugola should be a Lawnmower created by
+  Frank Lautenschlaeger, Daniel Grunow and
+  Simon Golpe Varela.
+  It is a end projekt for the "state technician school- Berlin"
+  This code is for the sensor part of the robot and was created
+  by Simon Golpe Varela on 09/26/2018.
+  Last change: 09/26/2018
+*/
+
+#ifndef __grugolasense_h
+#define __grugolasense_h
+
+#include <Arduino.h>
+
+//+++++Tell your board, which sourcecode comes from a .c file;
+        #ifdef __cplusplus
+        extern "C"{
+        #endif
+
+        extern int blinky();
+
+        #ifdef __cplusplus
+        }
+        #endif
+//+++++End of the sourcecode from a.c file
+
+#endif
