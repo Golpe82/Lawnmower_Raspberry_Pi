0f09a602aa75a37baade01a03c3af0eaabaf5c0f
Golpe82 <33048138+Golpe82@users.noreply.github.com>
2018-10-01 15:02:51 +0200

    Update grugolasense.c
    now with ultrasonic sensor measure

--- code for arduino/grugolasense.c
+++ code for arduino/grugolasense.c
@@ -7,22 +7,49 @@
   It is a end projekt for the "state technician school- Berlin"
   This code is for the sensor part of the robot and was created
   by Simon Golpe Varela on 09/26/2018.
-  Last change: 09/26/2018
+  Last change: 01/10/2018
 */
 
 #include <Arduino.h>
-#include <stdio.h>
 #include "grugolasense.h"
 
+///////////
+//DEFINES//
+///////////
 
-int blinky()
+//for ultrasonic function
+#define USTRIGGER 12
+#define USECHO 8
+#define USECHOVALUE digitalRead(8)//read ultrasonic echo value
+
+//Function for ultrasonic distance measure
+double ultrasonic()
 {
-  int blinkstate;
-  blinkstate=digitalRead(LED_BUILTIN);
-  digitalWrite(LED_BUILTIN, LOW);   
-  delay(1000);                      
-  digitalWrite(LED_BUILTIN, HIGH);
+  double USdistance,anfang,ende,signaltime;
 
-  return blinkstate;
+  digitalWrite(USTRIGGER, LOW); //be sure, trigger is low
+  delayMicroseconds(2);
+  digitalWrite(USTRIGGER, HIGH); //put trigger high
+  delayMicroseconds(10); //for 10 us
+  digitalWrite(USTRIGGER,LOW); //put trigger low
+  anfang=micros();
+  
+  while(!USECHOVALUE)//wait for rising edge
+  {
+      anfang=micros();
+      delayMicroseconds(5);
+  }
+
+  while(USECHOVALUE)//wait for falling edge
+  {
+      ende=micros();
+      delayMicroseconds(5);
+  }
+
+  signaltime=ende-anfang;  //calculate the time of the signal
+  USdistance=(signaltime/2)*0.03432;  //calculate the distance in cm
+
+  return USdistance;
 }
+
 
